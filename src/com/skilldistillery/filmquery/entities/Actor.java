72bc312b832ffbea5524898a5594607fefa03f59
Luke Sutherland <luke.sutherland@live.com>
2020-03-06 16:44:01 -0700

    init commit

---
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Actor {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+
+}
---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	
+	private int id;
+	private String title;
+	private String description;
+	private Integer releaseYear;
+	private int languageID;
+	private double rentalRate;
+	private Integer length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	
+}
